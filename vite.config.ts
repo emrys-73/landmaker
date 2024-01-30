c06b909fde3eae824bf8e5a4686bae078cda7d98
Adrian Montoya <montoya@astralta.com>
2024-01-30 18:35:10 +0100

    i honestly don't know what m doing

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	// build: {
-	// 	target: 'es2020' 
-	//   },
+	build: {
+		target: 'esnext' 
+	  },
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
