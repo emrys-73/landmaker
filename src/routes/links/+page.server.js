a4ab50a67f7e39eea274faff89487b9ed99c46ed
Adrian Montoya <montoya@astralta.com>
2024-01-31 11:02:23 +0100

    Protect links route

---
+++ src/routes/links/+page.server.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
+
+import { redirect } from '@sveltejs/kit';
+
+export const load = ({ locals }) => {
+    if (!locals.user) {
+        throw redirect(303, '/login')
+    }
+}
